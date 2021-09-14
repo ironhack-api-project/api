403c9cbf9e772fbd0c2da0aeb3a7f549525c46de
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-14 12:19:58 -0400

    geolocation function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Home(props) {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      //   x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  };
+  const showPosition = (position) => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return <div>HOME</div>;
 }
 
