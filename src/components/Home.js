bee86413feb895f134046e33533e43d1adc439d2
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-09-14 12:28:41 -0400

    ll

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ function Home(props) {
   return (
   <div>
     <span className="tagline">Let us help you find plans...</span>
+    <div className="boxes">
   <form>
     <input type="text" placeholder="City"/>
     <input type="text" placeholder="Zip Code"/>
-  </form>
-  <form>
     <input type="text" placeholder="Date"/>
     <input type="text" placeholder="Radius"/>
     </form>
+  </div>
   <button onClick="">Search</button>
   </div>
   );
