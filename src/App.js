61b904386f044c31e7311822e6bd5565edc0ecef
michaelporreca <michaelporreca@gmail.com>
2021-09-14 11:40:48 -0700

    added ticket image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Events from "./components/Events";
+import ticket from './ticket.png';
 
 //Ticketmaster API Key = biW1fGE1aeVKqhiGWAdGttCRSItyVN2z
 
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <span className="ticket">🎟️</span>
+        <span className="ticket"><img src={ticket} width="50" alt="ticket" /></span>
         <h1>getBooked</h1>
       </header>
 
