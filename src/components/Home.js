6601a385e5f148afa5a162e439543c6aa7c0382e
michaelporreca <michaelporreca@gmail.com>
2021-09-14 09:19:11 -0700

    added search css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,18 @@ import React from "react";
 
 function Home(props) {
   return (
-  <div>Let us help you find plans...
-<form>
-  <input type="text" placeholder="City"/>
-  <input type="text" placeholder="Zip Code"/>
-</form>
-<form>
-  <input type="text" placeholder="Date"/>
-  <input type="text" placeholder="Radius"/>
+  <div>
+    <span className="tagline">Let us help you find plans...</span>
+  <form>
+    <input type="text" placeholder="City"/>
+    <input type="text" placeholder="Zip Code"/>
   </form>
+  <form>
+    <input type="text" placeholder="Date"/>
+    <input type="text" placeholder="Radius"/>
+    </form>
   <button onClick="">Search</button>
-</div>
+  </div>
   );
 }
 
