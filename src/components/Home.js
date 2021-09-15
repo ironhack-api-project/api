c53a54fd195fcfde9e71b8aa5fcd277af4979e17
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-15 15:04:36 -0400

    showevents not found page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ function Home(props) {
   let [events, setEvents] = useState([]);
 
   const handleSubmit = (e) => {
+    console.log("button");
     e.preventDefault();
     let eventKeyWord = e.target[0].value;
     let eventCity = e.target[1].value;
@@ -23,46 +24,50 @@ function Home(props) {
       "&apikey=biW1fGE1aeVKqhiGWAdGttCRSItyVN2z";
 
     axios.get(linkToAxios).then((resApi) => {
-      setEvents(resApi.data._embedded.events);
+      setEvents(resApi?.data?._embedded?.events);
     });
   };
 
   const ShowEvents = () => {
-    return (
-      <div className="events">
-        {events.map((uniqueEvent) => {
-          let img = [...uniqueEvent.images].filter((im) => im.width > 1000);
-          let city = [...uniqueEvent._embedded.venues].map((venue) => {
-            return venue.city;
-          });
-          return (
-            <div key={uniqueEvent.id}>
-              <Link
-                to={{
-                  pathname: `/events/${uniqueEvent.id}`,
-                  myCustomProps: uniqueEvent,
-                }}
-              >
-                <img src={img[0].url} width="500" />
-                <div>
-                  <h2>{uniqueEvent.name}</h2>
-                </div>
-                <div>
-                  <strong>City</strong>: {city[0].name}
-                </div>
+    if (events === undefined) {
+      return <div>not found</div>;
+    } else {
+      return (
+        <div className="events">
+          {events?.map((uniqueEvent) => {
+            let img = [...uniqueEvent.images].filter((im) => im.width > 1000);
+            let city = [...uniqueEvent._embedded.venues].map((venue) => {
+              return venue.city;
+            });
+            return (
+              <div key={uniqueEvent.id}>
+                <Link
+                  to={{
+                    pathname: `/events/${uniqueEvent.id}`,
+                    myCustomProps: uniqueEvent,
+                  }}
+                >
+                  <img src={img[0].url} width="500" />
+                  <div>
+                    <h2>{uniqueEvent.name}</h2>
+                  </div>
+                  <div>
+                    <strong>City</strong>: {city[0].name}
+                  </div>
 
-                <div>
-                  <strong>Time</strong>: {uniqueEvent.dates.start.localTime}
-                </div>
-                <div>
-                  <strong>Date</strong>: {uniqueEvent.dates.start.localDate}
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    );
+                  <div>
+                    <strong>Time</strong>: {uniqueEvent.dates.start.localTime}
+                  </div>
+                  <div>
+                    <strong>Date</strong>: {uniqueEvent.dates.start.localDate}
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
   };
 
   return (
