e799ca65f5c82c28c0c72126f892702f1cf29759
Bruno Barbagelata <bruno140685@gmail.com>
2021-09-16 16:44:42 -0400

    home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -77,7 +77,6 @@ function Home(props) {
                   to={{
                     pathname: `/events/${uniqueEvent.id}`,
                     myCustomProps: uniqueEvent,
-                    lat: latitude,
                   }}
                 >
                   <img src={img.url} className="resultimage" />
